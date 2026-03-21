5b0593a281595ae8b4c3f1d9436eb16b179d8703
20222ireads0016 <20222ireads0016@ifba.edu.br>
2026-03-21 15:19:34 -0300

    Initial Commit

---
+++ Backend/src/main/java/br/com/ifba/pgr04paulohenrique/Pgr04PaulohenriqueApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifba.pgr04paulohenrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pgr04PaulohenriqueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pgr04PaulohenriqueApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/br/com/ifba/pgr04paulohenrique/Pgr04PaulohenriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifba.pgr04paulohenrique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pgr04PaulohenriqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
